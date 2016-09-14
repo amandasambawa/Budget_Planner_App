2c88eb75843f3bf9d1543f0af3b6d721dd98df95
amandasambawa <amandasambawa@gmail.com>
2016-09-13 21:39:11 -0700

    Budget Planner App
    A budget planning iOS app using Swift 2.0.

---
+++ Budget Planner Final/SecondViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SecondViewController.swift
+//  Budget Planner Final
+//
+//  Created by Amanda Sambawa on 9/9/16.
+//  Copyright © 2016 Amanda. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let blueColor = UIColor(red: 129.0/255.0, green: 195.0/255.0, blue: 226.0/255.0, alpha: 1.0)
+        
+        view.backgroundColor = blueColor
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
